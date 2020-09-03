969033086abcda639c4541d8c993d0d46385e63c
zhanghx0905 <731931282@qq.com>
2020-09-03 16:09:04 +0800

    载入界面

---
+++ app/src/main/java/com/example/zhanghx/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.example.zhanghx
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+
+@SuppressLint("StaticFieldLeak")
+lateinit var GLOBAL_CONTEXT: Context
+
+
+// 载入界面，用于初始化数据和权限
+class SplashActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val PERMISSIONS_STORAGE =
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        GLOBAL_CONTEXT = applicationContext
+
+        while (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
+        }
+        initGlobals()
+        val dstIntent = Intent(this, ListActivity::class.java)
+        startActivity(dstIntent)
+        finish()
+    }
+}
+
+fun initGlobals() {
+    // TODO
+}
